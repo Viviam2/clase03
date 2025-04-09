a3748c9f93023b228da6efc592b36b3a588ad876
Viviam La Torre <viviam20lt@gmail.com>
2025-04-09 18:39:07 -0500

    Cambios SQL ETL 01

--- etl_sql.py
+++ etl_sql.py
@@ -1,1 +1,8 @@
 ## Script de inhesta de SQL a DataLake
+
+import pandas as pd
+
+df = pd.read_csv('input.csv')
+df['processed'] = df['value'] * 10
+df.to_csv('output4.csv', index=False)
+print("ETL completado.")
